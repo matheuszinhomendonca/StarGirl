06704fbb05b8426857e7bed1ef97c37c78459927
Matheus Mendonca Ferreira <rm90499@estudante.fieb.edu.br>
2023-06-29 15:47:24 -0300

    29/06/2023

---
+++ Assets/Script/Ataque.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ataque : MonoBehaviour
+{
+
+    public float velocLaser = 12.0f ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(Vector3.right * velocLaser * Time.deltaTime);
+
+       if ( transform.position.x > 5.3f ){
+           Destroy(this.gameObject);
+       }
+    }
+}
